f58119d6441c9242dfc57e7155adbf3a6ce959a3
Bong Lee <bongster228@github.com>
2021-03-29 14:39:23 -0700

    implement infite scroll when going through search results (closes #19). <frontend>

--- client/src/components/search/SearchResults.tsx
+++ client/src/components/search/SearchResults.tsx
@@ -1,9 +1,11 @@
-import { Grid } from "@material-ui/core";
-import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import { Button, Grid, Typography } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { RootState } from "app/rootReducer";
-import React from "react";
+import { useAppDispatch } from "app/store";
 import { shallowEqual, useSelector } from "react-redux";
+import styled from "styled-components";
 import SearchResultItem from "./SearchResultItem";
+import { loadNextPage } from "./searchSlice";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,14 +19,25 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const LoadMoreButton = styled(Button)`
+  margin-bottom: 2rem;
+  padding: 1rem;
+`;
+
 const SearchResults = () => {
   const classes = useStyles();
+  const dispatch = useAppDispatch();
 
-  const { searchResults } = useSelector((state: RootState) => {
+  const { searchResults, nextPageToken } = useSelector((state: RootState) => {
     return {
       searchResults: state.search.searchResults,
+      nextPageToken: state.search.nextPageToken || "",
     };
   }, shallowEqual);
+
+  const handleLoadNextPage = () => {
+    dispatch(loadNextPage({ nextPageToken }));
+  };
 
   return (
     <Grid container justify="center" className={classes.root}>
@@ -34,6 +47,16 @@ const SearchResults = () => {
           searchResults.map((result) => (
             <SearchResultItem key={result.placeId} result={result} />
           ))}
+
+        {nextPageToken && (
+          <LoadMoreButton
+            onClick={handleLoadNextPage}
+            variant="outlined"
+            color="primary"
+          >
+            <Typography variant="h6">Load More Places</Typography>
+          </LoadMoreButton>
+        )}
       </Grid>
     </Grid>
   );
