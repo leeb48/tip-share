bedaf00f85d78721edff6bd576140f900e98403f
Bong Lee <bongster228@github.com>
2021-03-29 13:31:47 -0700

    include images to the search results (closes #21). <backend>

--- server/src/main/java/com/projects/tipshare/config/WebConfig.java
+++ server/src/main/java/com/projects/tipshare/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.projects.tipshare.config;
 
+import com.google.maps.GeoApiContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +9,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private final GooglePlacesProperties googlePlacesProperties;
+
+    public WebConfig(GooglePlacesProperties googlePlacesProperties) {
+        this.googlePlacesProperties = googlePlacesProperties;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*");
+    }
+
+
+    /**
+     * Creates the GeoApiContext bean that is used when working with Google Places API
+     *
+     * @return GeoApiContext with api key
+     */
+    @Bean
+    public GeoApiContext geoApiContext() {
+        return new GeoApiContext.Builder()
+                .apiKey(googlePlacesProperties.getApiKey())
+                .build();
     }
 }
--- server/src/main/java/com/projects/tipshare/controller/TestController.java
+++ server/src/main/java/com/projects/tipshare/controller/TestController.java
@@ -1,27 +1,23 @@
 package com.projects.tipshare.controller;
 
-import com.projects.tipshare.config.GooglePlacesProperties;
+import com.google.maps.errors.ApiException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("api/test")
 public class TestController {
 
-    private final GooglePlacesProperties googlePlacesProperties;
-
-    public TestController(GooglePlacesProperties googlePlacesProperties) {
-        this.googlePlacesProperties = googlePlacesProperties;
-    }
 
     @GetMapping
-    public ResponseEntity<?> test() {
+    public ResponseEntity<?> test() throws InterruptedException, ApiException, IOException {
 
-        System.out.println(googlePlacesProperties.getApiKey());
 
-        return ResponseEntity.ok("okay");
+        return ResponseEntity.ok("ok");
     }
 
     @GetMapping("/user")
